b001ee5825fd4f93ed4db98d758d369a244dcaee
HASSANENGI <h.m241202@gmail.com>
2025-05-06 11:00:19 +0500

    Added opeartors file

---
+++ operators.js
@@ -0,0 +1,77 @@
+let x=20;
+let y=10;
+
+// Arithmetic Operators
+
+let a=x+y // Addition
+
+console.log(a);
+
+let s=x-y;  // Sutraction
+
+console.log(s);
+
+let m=x*y;  // Multiplication
+
+console.log(m);
+
+let d=x/y   // Diviison
+
+console.log(d);
+
+let e= x**y  // Exponentiation
+
+console.log(e);
+
+let r= x%y  // Modulus (Division Remainder)
+
+console.log(r)
+
+let i= x++;  // increment 
+
+console.log(i);
+
+let l= y--  // decrement 
+
+console.log(l);
+
+// Logical Operators
+
+let t= true;
+
+let f= false;
+
+let and = t && f;  // logical AND
+
+console.log(and); // output: false 
+
+let or = t || f ;   // logical OR
+
+console.log(or);    // output: true
+
+let not = !t    // logical not
+
+console.log(not);  // output: false
+
+// Comparison Operators
+
+console.log(x==y);  // equal  value (output: false)
+
+console.log(x===y);   // equal value snd equal type (output: false)
+
+console.log(x!=y);  // not equal (output: true)
+
+console.log(x > y);  // x greater than y  (output: true)
+
+console.log(x < y); // x less than y (output: false)
+
+console.log (x >= y);   // x greater than or equal to y
+
+console.log(x <= y)     // x less than or equal to y
+
+console.log(x ? x : y) // ternary operator( shortcut for an if-else statement ): condition ? value_if_true : value_if_false; 
+//  here x=21  is a truthy value (anything other than 0, false, null, undefined, NaN, or "" is considered true in condition checks)
+
+
+
+
